3130dbe4f3c4a8118867bc93e1d25a5b724c5b0d
bob <bob@bob.com>
2025-01-02 13:25:04 -0800

    working, tests passing

--- src/riot.py
+++ src/riot.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 from Cheetah.Template import Template
 import itertools as itt
 import os
+from multidict import MultiDict
 
 
 # deal with duplicate keys
@@ -12,17 +13,25 @@ class MOD(dict):
         super().__init__(*args, **kwargs)
          
     def __setitem__(self, k, v):
-        if k == 'textsize':
+        print(f"k:{k} v: {v}")
+        t = self.get(k)
+        if t is not None and len(t) >0:  # if key exists, create new key with unique number appended to it. 
             n = next(self.c)
-            k = k + str(n)
-        #print('setitem:', id(self), k, v)
-        super().__setitem__(k, v)
+            new_key = f"{k}_____{n}"
+            if(t[0] == v):
+                #print(f"HOLY SHIT WHY?  k: {k} v: {v}")
+                super().__setitem__(k, v)
+            else: 
+                super().__setitem__(new_key, v)
+            #print("i hate you python")
+        else:
+            super().__setitem__(k, v)
 
 
 class Riot:
     def __init__(self):
         self.ini_config = {}
-        self.config_dict = {}
+        self.ini_config_dict = {}
 
         # default template directory
         #  could have a config somewhere for this
@@ -31,36 +40,59 @@ class Riot:
         self.ini_fpath = ""
 
     def read_ini(self, fpath):
-        #config = ConfigParser(strict=False, dict_type=multidict )
+        print(f"opening file: {fpath}  ")
+
         self.ini_fpath = fpath;
         config = ConfigParser(
             strict = False,
             dict_type = MOD,
-            allow_no_value=True)
-        config.read(fpath)
+            empty_lines_in_values = False
+            #allow_no_value=True
+            )
+
+        conf_string = ""
+        if(not os.path.exists(fpath)):
+            print("file does not exist")
+            return None
+        else:
+            with open(fpath) as f:
+                conf_string = f.read()
+    
+
+        config.read_string(conf_string)
+
+
+        tmp_config = {}
         for section in config.sections():
             section_data = {}
-            #print(f"section {section}")
-            self.config_dict[section] = {}
+            #section_data = MOD()
+            print(f"section {section}")
             for key, value in config.items(section):
-                if key in self.config_dict[section]:
-                    print(f"Duplicate key '{key}' in section '{section}' found. Skipping.")
-                else:
-                    section_data[key] = value
-            self.config_dict[section] = section_data
+                section_data[key] = value
+            tmp_config[section] = section_data
+        print("read_ini: file parsed")
         self.ini_config = config
-        #print(f"Config Dict: {self.config_dict}")
+        self.ini_config_dict = tmp_config
+
+    def ini_conf_to_string(self,section_name,header=True):
+        output = []
+        if header:
+            output = [ f"[{section_name}]"]
+
+        for key, value in self.ini_config_dict[section_name].items():
+            if "_____" in key:
+                [a,b ]= key.split("_____")
+                output.append(f"{a.upper()} = {value}")
+            else:
+                output.append(f"{key.upper()} = {value}")
+        return "\n".join(output)
+
+
 
     def write(self, fname, content):
         with open(fname, 'w') as f:
             f.write(content)
 
-    def parse_ini_template(self,section_name):
-        output = []
-        for key, value in self.config_dict[section_name].items():
-            output.append(f"{key.upper()} = {value}")
-            
-        return "\n".join(output)
 
 
     def loadTemplate(self, template_file_path,context):
@@ -88,14 +120,17 @@ class Riot:
                     template_file_path = os.path.join(root, file)
                     print(f"found template: {template_file_path}")
                     section_name = os.path.splitext(file)[0]
-                    gen_string = self.parse_ini_template(section_name)
+                    gen_string = self.ini_conf_to_string(section_name,header=False)
                     context = {
                         "riogen": gen_string
                     }
                     generated_section = self.loadTemplate(template_file_path,context)
                     templates[section_name] = generated_section
+
+
+        print("processing sections")
         for section in self.ini_config.sections():
-            
+            print(f"section {section}") 
             if section in templates.keys():
                 print(f"found generated section template: {section}")
                 s = templates[section] 
@@ -104,16 +139,13 @@ class Riot:
 
             else:
                 print(f" {section} template not found, leaving alone")
-                output = [f"[{section.upper()}]\n"]
-                for key, value in self.config_dict[section].items():
-                    output.append(f"{key.upper()} = {value}")
-                s = "\n".join(output)
+                s = self.ini_conf_to_string(section)
                 print( s)
                 new_config.append(s)
 
         new_config_string = "\n".join(new_config)
         new_config_fname = "thenew.ini"
-        print("full config generated as file {new_config_fname} \n")
+        print(f"full config generated as file {new_config_fname} \n")
         with open(new_config_fname, 'w') as file:
             file.write(new_config_string)
 
--- test/test_riot.py
+++ test/test_riot.py
@@ -11,8 +11,8 @@ def create_ini_file(content):
 class TestRiot:
     def setup_method(self):
         # display template
-        self.t1 = "test/DISPLAY.t"
-        self.ini_file_path = "test/rio-pure.ini"
+        self.t1 = "./test/DISPLAY.t"
+        self.ini_file_path = "./test/rio-pure.ini"
 
     def teardown_method(self):
         None
@@ -20,9 +20,12 @@ class TestRiot:
     def test_read_ini(self):
         riot = Riot()
         riot.read_ini(self.ini_file_path)
-        
-        print(f" filter section: {riot.config_dict["FILTER"]}")
-        riot.config.get("DISPLAY")
+        #print(f"keys: {riot.config_dict.keys()}") 
+        for key in riot.ini_config_dict.keys():
+            print(f"key: {key}")
+        #print(f"display section: {riot.config_dict["DISPLAY"]}") 
+        print(f" filter section: {riot.ini_config_dict["FILTER"]}")
+        #riot.ini_config.get("DISPLAY")
 
     def test_parse_template_dir(self):
         riot = Riot()
@@ -34,7 +37,7 @@ class TestRiot:
         riot = Riot()
         riot.read_ini(self.ini_file_path)
         section_name = "DISPLAY"
-        gen_string = riot.parse_ini_template(section_name) 
+        gen_string = riot.ini_conf_to_string(section_name,header=False) 
 
         context = {
             "riogen": gen_string
